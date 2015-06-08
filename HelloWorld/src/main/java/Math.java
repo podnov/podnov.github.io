70e9a81aa55dd6ab612ad2f8c6271683b359a271
Ben Crawford <ben_crawford23@yahoo.com>
2015-06-08 18:39:36 -0500

    Multiply Added

--- HelloWorld/src/main/java/Math.java
+++ HelloWorld/src/main/java/Math.java
@@ -7,5 +7,9 @@ public class Math {
     public int subtract(int left, int right) {
         return left - right;
     }
+    
+    public int multiply( int left, int right){
+    	return left * right;
+    }
 
 }
